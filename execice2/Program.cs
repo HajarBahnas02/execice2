c7ed6f80e7caf32d9de0b9079956173e391b6530
HajarBahnas02 <hajar.bahnas@usmba.ac.ma>
2022-10-31 11:16:10 +0100

    Add project files.

---
+++ execice2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace execice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                int[] t = { 2, 9, 14, 18, 26 };
+                int i;
+                Console.WriteLine("entrez un nombre:");
+                int x = Convert.ToInt32(Console.ReadLine());
+                t[4] = x;
+                for (i = 4; i > 0; i--)
+                {
+                    if (t[i - 1] > x)
+                    {
+                        t[i] = t[i - 1];
+                        t[i - 1] = x;
+                    }
+                }
+                for (i = 0; i < 5; i++)
+                {
+                    Console.Write("\t" + t[i]);
+                }
+                  Console.ReadLine();
+        }
+        }
+    }
+
+
+   
